4798995e1618ba5d2863c9ab9b935040d7e0e770
Rafael Gomes <rgomes@dnsfilter.com>
2024-05-31 15:50:31 -0300

    adicionando o dia 02 no git

---
+++ dia_02/aula_05_2_terraform_remote_state/sg.tf
@@ -0,0 +1,31 @@
+data "aws_vpc" "selected" {
+  tags = {
+    Environment = "production"
+  }
+}
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = data.aws_vpc.selected.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "${data.terraform_remote_state.aula_output.outputs.instance_ip_addr}/32"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
