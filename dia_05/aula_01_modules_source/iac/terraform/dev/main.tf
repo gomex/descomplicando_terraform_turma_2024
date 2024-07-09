b9de306410e372a523658b2fb15daf3824307bb1
Rafael Gomes <rgomes@dnsfilter.com>
2024-07-09 17:46:08 -0300

    fix: Adicionando mudanças da aula ao vio

--- dia_05/aula_01_modules_source/iac/terraform/dev/main.tf
+++ dia_05/aula_01_modules_source/iac/terraform/dev/main.tf
@@ -1,5 +1,6 @@
-module "projetoa" {
-  source      = "git@github.com:gomex/modulo_instancias.git//modules/instancia?ref=v2.0.0"
-  nome        = "projeto_alpha"
-  environment = "Dev"
+module "projeto_alpha" {
+  source      = "git@github.com:gomex/modulo_instancias.git?ref=v2.0.0"
+  nome        = var.nome
+  environment = var.environment
 }
+
